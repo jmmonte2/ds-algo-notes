bd1bdce1ae36e96d14af4863f24c157d884c451d
Jacob Montemayor <jacob.montemayor@live.com>
2022-02-07 16:54:43 -0600

    Implement stack

---
+++ Stack/implementation_stack.py
@@ -0,0 +1,21 @@
+# Assuming end of list will be top of the stack
+
+
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def is_empty(self):
+        return self.items == []
+    
+    def push(self, item):
+        self.items.append(item)
+    
+    def pop(self):
+        return self.items.pop()
+
+    def peek(self):
+        return self.items[len(self.items)-1]
+
+    def size(self):
+        return len(self.items)
